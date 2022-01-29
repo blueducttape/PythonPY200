e19bf8beaae0eccabde4b81d5af4ab9604f657d5
uliana <val.uliana@yandex.ru>
2022-01-29 12:43:15 +0300

    car_factory

--- Инкапсуляция, наследование, полиморфизм/Повторение/car_factory/car.py
+++ Инкапсуляция, наследование, полиморфизм/Повторение/car_factory/car.py
@@ -12,19 +12,19 @@ class DriverTypeError(Exception):
     pass
 
 
-class EngineIsNotRunning(Exception):
+class EngineIsNotRunning(Exception):   # двигатель не запущен
     pass
 
 
-class DoTechnicalDiscussion(Exception):
+class DoTechnicalDiscussion(Exception):    # ТО не пройден
     pass
 
 
-class DriverNotFoundError(Exception):
+class DriverNotFoundError(Exception):    # водитель не найден
     pass
 
 
-class EngineIsRunning(Exception):
+class EngineIsRunning(Exception):     # двигатель запущен
     pass
 
 
@@ -34,55 +34,58 @@ class Car:
     __created_car = 0
 
     def __init__(self, engine_type=None, body_type=None, gear_type=None,
-                 drive_type=None, configuration=None, color=None):
+                 drive_type=None, configuration=None, color=None) -> None:
 
-        self.__body_type = body_type
-        self._engine_type = engine_type
-        self._gear_type = gear_type
-        self._drive_type = drive_type
-        self.configuration = configuration
-        self.color = color
+        self.__body_type = body_type    # тип кузова
+        self._engine_type = engine_type     # тип двигателя
+        self._gear_type = gear_type     # тип коробки передач
+        self._drive_type = drive_type   # тип привода
+        self.configuration = configuration  # комплектация
+        self.color = color  # цвет
 
-        self.__vin_number = uuid.uuid4()
-        self.__created_time = time.time()
-        self.__mileage = 0
+        self.__vin_number = uuid.uuid4()    # vin (идентификационный номер транспортного средства)
+        self.__created_time = time.time()   #
+        self.__mileage = 0      # пробег
 
-        self.__status_engine = False
-        self.__driver = None
+        self.__status_engine = False    # статус двигателя
+        self.__driver = None    # водитель
         self.__last_to = 0  # пробег на котором было сделано последнее ТО
         self.__service_interval = 30  # необходимая частота ТО
 
         self.car_key = None  # ключ который хранится в машине, и сверяется с ключом водителя
         self.__keys_was_send = False  # переменная которая проверяет выдавались ли ключи
 
-    def __new__(cls, *args, **kwargs):
+    def __new__(cls, *args, **kwargs) -> object:   # новые объекты класса
         cls.__append_new_car_counter()
-        print(f"Создано {cls.__created_car} машина класса {cls.__name__}")
+        print(f"Создано {cls.__created_car} машин класса {cls.__name__}")
         return super().__new__(cls)
 
+    # =============
+    # Методы класса
+    # =============
     @classmethod
-    def change_brand(cls, new_brand):
+    def change_brand(cls, new_brand) -> None:    # изменить марку машины
         cls.brand = new_brand
 
     @classmethod
-    def _set_max_speed(cls, max_speed):
+    def _set_max_speed(cls, max_speed) -> int | float | Exception:  # установка максимальной скорости
         if not isinstance(max_speed, (int, float)):
             raise TypeError(
                 f'Ожидается тип {int} или {float}, получен {type(max_speed)}')
         cls._max_speed = max_speed
 
     @classmethod
-    def __append_new_car_counter(cls):
+    def __append_new_car_counter(cls) -> None:  # счетчик объектов класса
         cls.__created_car += 1
 
-    def start_engine(self, key):
+    def start_engine(self, key) -> None:    # запуск двигателя
         if self.__check_keys(key):
             self.__status_engine = True
             print('Машина завелась')
         else:
             print('Крутится стартер')
 
-    def __is_ready_move(self):
+    def __is_ready_move(self) -> bool:  # проверяет готовность автомобиля к движению
         if not self.__status_engine:
             raise EngineIsNotRunning("двигатель не запущен")
         if self.__driver is None:
@@ -90,14 +93,14 @@ class Car:
 
         return True
 
-    def __create_keys(self):
+    def __create_keys(self) -> str:    # создание ключа
         h = hashlib.new('sha256')
         vin = str(self.__vin_number).encode('utf-8')
         h.update(vin)
         self.car_key = h.hexdigest()
         return self.car_key
 
-    def get_keys(self):
+    def get_keys(self) -> str:  # проверяет, выданы ли ключи
         """Ключи выдаются только 1 раз"""
         if self.__keys_was_send:
             print('Ключи уже были выданы')
@@ -105,14 +108,14 @@ class Car:
             self.__keys_was_send = True
             return self.__create_keys()
 
-    def __check_keys(self, key):
+    def __check_keys(self, key) -> bool:    # проверяет, подходят ли ключи
         if self.car_key == key:
             return True
         else:
             print('Ключи не подходят')
             return False
 
-    def move(self, distance=10):
+    def move(self, distance=10) -> str | Exception:     # движение автомобиля
         try:
             if self.__is_ready_move():
                 for i in range(distance):
@@ -133,21 +136,25 @@ class Car:
         except (EngineIsNotRunning, DriverNotFoundError) as e:
             print(f"Машина не может поехать, т.к. {e}")
 
+    # ==================
+    # Статические методы
+    # ==================
     @staticmethod
-    def move_direction(direction):
+    def move_direction(direction) -> dict:  # выбирает направление движения
         direction_dict = {0: "прямо", 1: "налево", 2: "направо", 3: "разворот"}
         print(f"Автомобиль выполняет движение: {direction_dict.get(direction, 'прямо')}")
 
-        #print(f"Угол поворота = {angle}")
-        #if angle < -180:
-            #print("Поворот налево")
-        #elif angle == 0:
-            #print ("Автомобиль едет прямо")
-        #elif angle < 180:
-            #print("Поворот направо ")
-        #elif angle >= 180:
-            #print("Разворот")
+    #        print(f"Угол поворота = {angle}")
+    #    if angle < -180:
+    #        print("Поворот налево")
+    #    elif angle == 0:
+    #        print ("Автомобиль едет прямо")
+    #    elif angle < 180:
+    #        print("Поворот направо ")
+    #    elif angle >= 180:
+    #        print("Разворот")
 
+    # Эквивалент свойствам (property)
     # def set_driver(self, driver: Driver):
     #     if not isinstance(driver, Driver):
     #         raise DriverTypeError(f"Ожидается тип {Driver}, получен {type(driver)}")
@@ -157,26 +164,26 @@ class Car:
     #     return self.__driver
 
     @property
-    def driver(self):
+    def driver(self) -> Driver:   # водитель
         return self.__driver
 
     @driver.setter
-    def driver(self, driver: Driver):
+    def driver(self, driver: Driver) -> Driver | Exception:     # сеттер для водителя
         if not isinstance(driver, Driver):
             raise DriverTypeError(
                 f"Ожидается тип {Driver}, получен {type(driver)}")
         self.__driver = driver
 
-    def get_mileage(self):
+    def get_mileage(self) -> int:   # пробег
         return self.__mileage
 
-    def _set_mileage(self, mileage):
+    def _set_mileage(self, mileage) -> int | float:     # сеттер для пробега
         if not isinstance(mileage, (int, float)):
             raise TypeError(
                 f'Ожидается тип {int} или {float}, получен {type(mileage)}')
         self.__mileage = mileage
 
-    def check_technical_discussion(self) -> bool:
+    def check_technical_discussion(self) -> bool:   # проверка необходимости техосмотра
         if self.__mileage - self.__last_to == 40:
             return False
         elif self.__mileage - self.__last_to == 20:
@@ -185,11 +192,11 @@ class Car:
             print(f"Сейчас необходимо сделать ТО")
         return True
 
-    def do_technical_discussion(self):
+    def do_technical_discussion(self) -> str:   # ТО
         self.__last_to = self.__mileage
-        print("Очередное ТО пройден")
+        print("Очередной ТО пройден")
 
-    def autostart_engine(self):
+    def autostart_engine(self) -> bool:     # автозапуск двигателя
         if not self.__status_engine:
             self.__status_engine = True
             return True
@@ -202,7 +209,6 @@ class Honda(Car):
     brand = "Honda"
 
     def __init__(self):
-
         super().__init__()
 
 
